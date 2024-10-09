40e0a76b908d34a6173c090dc4e1a3cc395ea782
rickjarzy <peru77@gmx.at>
2024-10-09 12:59:02 +0200

    upload speedtest

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+import traceback
+from datetime import datetime
+from speedtest import Speedtest
+
+def main() -> None:
+
+    try:
+
+        while True: 
+
+            speedtest = Speedtest()
+            time_now = datetime.now()
+            print("Start testing internet connection ...")
+            dl_speed = speedtest.download() / 1000000  # Convert to Mbps
+            ul_speed = speedtest.upload() / 1000000  # Convert to Mbps
+
+            print("Download Speed: {:.2f}".format(dl_speed) + f" Mbps\t- {time_now}")
+            print("Upload Speed: {:.2f}".format(ul_speed) + f" Mbps\t- {time_now}")
+            time.sleep(300)
+
+    except Exception as e:
+        print(f"### ERROR - something went wrong {e}")
+        traceback.print_exc()
+    except speedtest.SpeedtestException as e:
+        print(f"### ERROR - during speedtest something went wrong: {e}")
+        traceback.print_exc()
+    except KeyboardInterrupt:
+        print("PROGRAM FINISHED by user")
+
+    print("Programm ENDE")
+
+
+if __name__ == "__main__":
+    main()
