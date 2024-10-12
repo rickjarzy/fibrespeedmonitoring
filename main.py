ee7d3dab8d3b1112e70a58a64c671e8693e34124
rickjarzy <peru77@gmx.at>
2024-10-12 11:10:44 +0200

    Refactor Error Handling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import traceback
 from datetime import datetime
-from speedtest import Speedtest, SpeedtestException
+from speedtest import Speedtest, SpeedtestException, ConfigRetrievalError
 
 
 def main() -> None:
@@ -13,10 +13,12 @@ def main() -> None:
             print("Start testing internet connection ...")
             dl_speed = speedtest.download() / 1000000  # Convert to Mbps
             ul_speed = speedtest.upload() / 1000000  # Convert to Mbps
+            best_servers = speedtest.get_best_server()
 
             print("Download Speed: {:.2f}".format(dl_speed) + f" Mbps\t- {time_now}")
             print("Upload Speed: {:.2f}".format(ul_speed) + f" Mbps\t- {time_now}")
-            time.sleep(300)
+            print(f"Best Server used: {best_servers}")
+            time.sleep(30)
 
     except Exception as e:
         print(f"### ERROR - something went wrong {e}")
@@ -24,13 +26,13 @@ def main() -> None:
         print("Restarting speedtest")
         time.sleep(30)
         main()
-    except speedtest.SpeedtestException as e:
+    except SpeedtestException as e:
         print(f"### ERROR - during speedtest something went wrong: {e}")
         traceback.print_exc()
         time.sleep(30)
         print("Restarting speedtest")
         main()
-    except speedtest.ConfigRetrievalError as cre:
+    except ConfigRetrievalError as cre:
         print(f"### ERROR - ConfigRetrievalError - {cre}")
         traceback.print_exc()
         time.sleep(30)
